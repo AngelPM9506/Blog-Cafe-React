b138b244e58b4bf2c3b288c76848257bd59b4e1c
AngelPM9506 <parra_mondragon@live.com>
2022-10-13 12:10:38 -0500

    empezando con database

---
+++ back/src/db/index.js
@@ -0,0 +1,28 @@
+const { NODE_ENV, DB_USER, DB_PASS, DB_BD, DB_HOST, DB_DRIVER } = require('dotenv').config().parsed;
+const { Sequelize, Op } = require('sequelize');
+const sequelize = NODE_ENV === 'production'
+    ? new Sequelize({
+        database: DB_BD,
+        dialect: DB_DRIVER,
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASS,
+        pool: {
+            max: 3,
+            min: 1,
+            idle: 100000
+        },
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            },
+            keepAlive: true
+        },
+        ssl: true
+    })
+    : new Sequelize(`${DB_DRIVER}://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_BD}`, {
+        logging: false,
+        native: false
+    });
